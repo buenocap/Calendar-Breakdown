65d24b0c6e5dc8ac3506a9cf76fb6e4782ab7e1b
Pedro Bueno <pbueno159@gmail.com>
2024-12-19 13:44:08 -0700

    first commit

---
+++ src/Calendar/Calendar.jsx
@@ -0,0 +1,346 @@
+import "./Calendar.css";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import CellContainer from "./CellContainer";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import uniqid from "uniqid";
+
+export default function Calendar(initalState) {
+  // Date State Control
+  const today = new Date();
+  const [selectedMonth, setSelectedMonth] = useState(today.getMonth() + 1);
+  const [selectedYear, setSelectedYear] = useState(today.getFullYear());
+
+  // Form State Control
+  const [formTitle, setFormTitle] = useState("");
+  const [formDescription, setFormDescription] = useState("");
+  const [formStartTime, setFormStartTime] = useState("");
+  const [formEndTime, setFormEndTime] = useState("");
+  const [formColor, setFormColor] = useState("#4285F4");
+  const [allDayEvents, setAllDayEvents] = useState(false);
+
+  // Test Data
+  const [userData, setUserData] = useState([
+    {
+      userID: "johnDoe",
+      age: 25,
+      events: [
+        {
+          eventID: uniqid(),
+          title: "Birthday",
+          description: "Happy Birthday",
+          startTime: "2024-12-05T10:00:00",
+          endTime: "2024-12-05T11:00:00",
+          allDay: false,
+          assignedColor: "orange",
+          owner: "johnDoe",
+        },
+        {
+          eventID: uniqid(),
+          title: "Second Birthday",
+          description: "Happy 2nd Birthday",
+          startTime: "2024-12-05T10:00:00",
+          endTime: "2024-12-05T23:45:00",
+          allDay: false,
+          assignedColor: "red",
+          owner: "johnDoe",
+        },
+      ],
+    },
+  ]);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  /**
+   * Handles the addition of a new event to the user's event list.
+   * Prevents the default form submission behavior, constructs a new event object
+   * using form data, and updates the user's event list with the new event.
+   * Finally, it closes the modal/form.
+   *
+   * @param {Object} e - The event object emitted by the form submission.
+   */
+  const handleAddEvent = (e) => {
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Create a new event object
+    let newEvent = {
+      eventID: uniqid(),
+      title: formTitle,
+      description: formDescription || "",
+      startTime: formStartTime || "",
+      endTime: formEndTime || "",
+      allDay: allDayEvents,
+      assignedColor: formColor || "#4285F4",
+      owner: e.target.formOwner?.value,
+    };
+
+    // Add the new event to the user's events
+    const updatedUserData = [...userData];
+    const userIndex = updatedUserData.findIndex(
+      (user) => user.userID === e.target.formOwner?.value
+    );
+    if (userIndex !== -1) {
+      updatedUserData[userIndex].events = [
+        ...updatedUserData[userIndex].events,
+        newEvent,
+      ];
+    }
+    setUserData(updatedUserData);
+    handleClose();
+
+    // Reset the form fields
+    setFormTitle("");
+    setFormDescription("");
+    setFormStartTime("");
+    setFormEndTime("");
+    setAllDayEvents(false);
+    setFormColor("#4285F4");
+  };
+
+  function handleMonthChange(e) {
+    setSelectedMonth(e.target.value);
+  }
+
+  function handleYearChange(e) {
+    setSelectedYear(e.target.value);
+  }
+
+  function renderCalendar() {
+    const month = selectedMonth;
+    const year = selectedYear;
+
+    const firstDayOfMonth = new Date(year, month - 1, 1);
+    const lastDayOfMonth = new Date(year, month, 0);
+
+    const totalDaysInMonth = lastDayOfMonth.getDate();
+    const firstWeekDayOfMonth = firstDayOfMonth.getDay();
+
+    const initialWeekDays = 8 - firstWeekDayOfMonth;
+    const initialBlankDays = 8 - initialWeekDays;
+
+    const createWeeks = () => {
+      const calendarRows = [];
+      let currentWeek = [];
+
+      // Add blank days at the start of the first week
+      for (let i = 0; i < initialBlankDays; i++) {
+        currentWeek.push(
+          <CellContainer type="blank" number={i} key={`blankCell-${i}`} />
+        );
+      }
+
+      // Add numbered days to the first week
+      for (let dayNum = 1; dayNum < initialWeekDays; dayNum++) {
+        const dayString = dayNum < 10 ? `0${dayNum}` : dayNum;
+        currentWeek.push(
+          <CellContainer
+            type="number"
+            number={dayNum}
+            userData={userData}
+            cellIdentifier={`${year}-${month}-${dayString}`}
+            key={`Cell-${year}-${month}-${dayNum}`}
+          />
+        );
+      }
+
+      calendarRows.push(<tr key={`week-0`}>{currentWeek}</tr>);
+
+      // Add remaining weeks
+      for (let dayNum = initialWeekDays; dayNum <= totalDaysInMonth; ) {
+        currentWeek = [];
+        for (let weekday = 0; weekday < 7; weekday++) {
+          if (dayNum > totalDaysInMonth) break;
+
+          const dayString = dayNum < 10 ? `0${dayNum}` : dayNum;
+          currentWeek.push(
+            <CellContainer
+              type="number"
+              number={dayNum}
+              userData={userData}
+              cellIdentifier={`${year}-${month}-${dayString}`}
+              key={`Cell-${year}-${month}-${dayNum}`}
+            />
+          );
+          dayNum++;
+        }
+        calendarRows.push(<tr key={`week-${dayNum / 7}`}>{currentWeek}</tr>);
+      }
+
+      return calendarRows;
+    };
+
+    return (
+      <Container fluid>
+        <Table>
+          <thead>
+            <tr className="days-in-week">
+              <th>Sun</th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+            </tr>
+          </thead>
+          <tbody>{createWeeks()}</tbody>
+        </Table>
+      </Container>
+    );
+  }
+
+  return (
+    <div className="body">
+      <div className="calendar-form">
+        <form className="selection-input">
+          <div>
+            <label htmlFor="month" className="input-label">
+              Month
+            </label>
+            <select
+              id="month"
+              value={selectedMonth}
+              onChange={handleMonthChange}
+            >
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="year" className="input-label">
+              Year
+            </label>
+            <input
+              type="number"
+              id="year"
+              value={selectedYear}
+              onChange={handleYearChange}
+            />
+          </div>
+        </form>
+        <Button variant="primary" onClick={handleShow}>
+          Add Event
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Event</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={(e) => handleAddEvent(e)}>
+              <Form.Group className="mb-3" controlId="formColor">
+                <Form.Label>Event Color</Form.Label>
+                <Form.Control
+                  type="color"
+                  value={formColor}
+                  onChange={(e) => setFormColor(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formTitle">
+                <Form.Label>Event Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Title"
+                  value={formTitle}
+                  onChange={(e) => setFormTitle(e.target.value)}
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formDescription">
+                <Form.Label>Event Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Description"
+                  value={formDescription}
+                  onChange={(e) => setFormDescription(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-2 allDay" controlId="formAllDay">
+                <Form.Label>All Day</Form.Label>
+                <Form.Check
+                  type="checkbox"
+                  value={allDayEvents}
+                  onChange={() => setAllDayEvents(!allDayEvents)}
+                />
+              </Form.Group>
+              {/* Only display date and time inputs if allDayEvents is false */}
+              {!allDayEvents && (
+                <>
+                  <Form.Group className="mb-3" controlId="formStartTime">
+                    <Form.Label>Start Date & Time</Form.Label>
+                    <Form.Control
+                      type="datetime-local"
+                      onChange={(e) => setFormStartTime(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formEndTime">
+                    <Form.Label>End Date & Time</Form.Label>
+                    <Form.Control
+                      type="datetime-local"
+                      onChange={(e) => setFormEndTime(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                </>
+              )}
+              {/* Only display date inputs if allDayEvents is true */}
+              {allDayEvents && (
+                <>
+                  <Form.Group className="mb-3" controlId="formStartTime">
+                    <Form.Label>Start Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      placeholder="Enter Start Date"
+                      onChange={(e) => setFormStartTime(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formEndTime">
+                    <Form.Label>End Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      placeholder="Enter End Date"
+                      onChange={(e) => setFormEndTime(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                </>
+              )}
+              <Form.Group controlId="formOwner" hidden>
+                <Form.Label>Owner</Form.Label>
+                <Form.Control type="hidden" value={userData[0].userID} />
+              </Form.Group>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit">
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
+      <div className="calendar">{renderCalendar()}</div>
+    </div>
+  );
+}
---
+++ src/Calendar/CellContainer.jsx
@@ -0,0 +1,195 @@
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Image } from "react-bootstrap";
+import "./CellContainer.css";
+export default function CellContainer({
+  type,
+  number,
+  userData,
+  cellIdentifier,
+}) {
+  const [show, setShow] = useState(false);
+  const [currentID, setCurrentID] = useState(0);
+  const [currentEvent, setCurrentEvent] = useState({});
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  var currentCalendarDay = toISOformat(new Date().toLocaleDateString());
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const headerImage = [
+    "Capybara1.jpeg",
+    "Capybara2.jpeg",
+    "Capybara3.jpeg",
+    "Capybara4.jpeg",
+  ];
+
+  function selectRandomImage() {
+    const randomIndex = Math.floor(Math.random() * headerImage.length);
+    setSelectedImage(headerImage[randomIndex]);
+  }
+
+  function displayEvent(id) {
+    setCurrentID(id);
+    let testing = userData.map((user) =>
+      user.events.find((event) => event.eventID === id)
+    );
+    setCurrentEvent(testing[0]);
+    selectRandomImage();
+    handleShow();
+    setCurrentEvent(testing[0]);
+  }
+
+  function resolveTimestamp(startTimeStamp, endTimeStamp, allDay) {
+    const Months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    // Check if the event is all day
+    if (allDay == true) {
+      let [year, month, day] = startTimeStamp?.split("-");
+      let [end, endMonth, endDay] = endTimeStamp?.split("-");
+
+      return `${Months[month - 1]} ${day}, ${year} - ${
+        Months[endMonth - 1]
+      } ${endDay}, ${end}`;
+    }
+
+    if (startTimeStamp && endTimeStamp) {
+      let [startDate, startTime] = startTimeStamp?.split("T");
+      let [endDate, endTime] = endTimeStamp?.split("T");
+
+      return formatTime(startDate, startTime, endDate, endTime, Months);
+    }
+
+    return 1;
+  }
+
+  function toISOformat(date) {
+    let [month, day, year] = date.split("/");
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    if (day < 10) {
+      day = "0" + day;
+    }
+
+    return `${year}-${month}-${day}`;
+  }
+
+  function formatTime(startDate, startTime, endDate, endTime, Months) {
+    if (startDate == endDate) {
+      const [year, month, day] = startDate?.split("-");
+      const [hr, min] = startTime?.split(":");
+      const [hr2, min2] = endTime?.split(":");
+      return `${Months[month - 1]} ${day}, ${year} • 
+      ${hr > 12 ? hr - 12 : hr}:${min} ${hr > 12 ? "PM" : "AM"} - ${
+        hr2 > 12 ? hr2 - 12 : hr2
+      }:${min2} ${hr2 > 12 ? "PM" : "AM"}`;
+    }
+
+    return `${startDate} • ${startTime} - ${endDate} • ${endTime}`;
+  }
+
+  switch (type) {
+    case "blank":
+      return <td className="blankCell" key={`blankCell-${number}`}></td>;
+
+    case "number":
+      return (
+        <>
+          <td className="numberCell" key={`numberCell-${number}`}>
+            <div
+              className={
+                cellIdentifier == currentCalendarDay ? "currentDay" : ""
+              }
+            >
+              {number}
+            </div>
+            <div>
+              {userData.map((user) => {
+                return user.events.map((event) => {
+                  let eventStartDate = event.startTime.split("T")[0];
+                  if (cellIdentifier === eventStartDate) {
+                    return (
+                      <div
+                        className="overflowControl"
+                        key={event.eventID}
+                        style={{
+                          backgroundColor: event.assignedColor,
+                          borderRadius: "5px",
+                          paddingLeft: "5px",
+                          margin: "1px",
+                          cursor: "pointer",
+                        }}
+                        id={event.eventID}
+                        onClick={(e) => displayEvent(e.target.id)}
+                      >
+                        {event.title}
+                      </div>
+                    );
+                  }
+                });
+              })}
+            </div>
+          </td>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+              <Modal.Title>
+                <Image src={`../../img/${selectedImage}`} fluid rounded />
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="icon-format-title">
+                <div
+                  style={{ backgroundColor: currentEvent?.assignedColor }}
+                  className="eventColor"
+                ></div>
+                <h3 className="eventTitle">{currentEvent?.title}</h3>
+              </div>
+              <div className="icon-format">
+                <i className="bi bi-calendar"></i>
+                <p>
+                  {resolveTimestamp(
+                    currentEvent?.startTime,
+                    currentEvent?.endTime,
+                    currentEvent?.allDay
+                  )}
+                </p>
+              </div>
+              <div className="icon-format">
+                <i className="bi bi-justify-left"></i>
+                <p>{currentEvent?.description}</p>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Edit
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      );
+
+    default:
+      console.log("Invalid cell type: " + type);
+      break;
+  }
+
+  return null;
+}
