954c43635af01182d2d5b382a9dcb9e53921db31
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2025-01-23 14:29:19 -0700

    Changed content layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,34 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import Card from "react-bootstrap/Card";
+
 import "./App.css";
 
 function App() {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <MenuBar />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={2}>
-          <div className="border shadow mb-2 mt-2 sidebar-container">
-            <Sidebar />
-          </div>
-        </Col>
-        <Col sm={10}>
-          <div className="border shadow mb-2 mt-2 calendar-container">
-            <Calendar />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Footer />
-      </Row>
-    </Container>
+    <>
+      <MenuBar />
+      <Container
+        fluid
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100dvh" }}
+      >
+        <Row>
+          <Col sm={2}>
+            <Card className="shadow sidebar-container">
+              <Sidebar />
+            </Card>
+          </Col>
+          <Col sm={10}>
+            <Card className="shadow">
+              <Calendar />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
