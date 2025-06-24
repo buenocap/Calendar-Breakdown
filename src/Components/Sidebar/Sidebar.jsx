256cd356c78b0f67bf17f4c0f832f76b41bc5cd7
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2025-06-24 15:02:48 -0600

    Feat: Add upcoming events and past event view
    User can now see their upcoming events sorted by the closest date to the furthest
    
    User can now see their past events sorted by newest past date (top) to oldest event (bottom)

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -10,49 +10,61 @@ export default function Sidebar({ userData, eventData }) {
 
   //Get date to sort out events
   const today = new Date();
-  const month = today.getMonth() + 1;
-  const day = today.getDate();
+  const todaysDate = today.toISOString().split("T")[0];
+
+  function upcomingEvents(events) {
+    const current = events.filter((event) => {
+      const eventDate = event.startDate;
+      return eventDate >= todaysDate;
+    });
+
+    return (
+      <Accordion>
+        {current.map((event) => (
+          <Accordion.Item eventKey={event._id} key={event._id}>
+            <Accordion.Header>{event.title}</Accordion.Header>
+            <Accordion.Body>{event.description}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    );
+  }
+
+  function pastEvents(events) {
+    const past = events.filter((event) => {
+      const eventDate = event.startDate;
+      return eventDate < todaysDate;
+    });
+
+    //Sort by date
+    past.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
+    return (
+      <Accordion>
+        {past.map((event) => (
+          <Accordion.Item eventKey={event._id} key={event._id}>
+            <Accordion.Header>{event.title}</Accordion.Header>
+            <Accordion.Body>{event.description}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    );
+  }
+
   return (
-    <div>
-      <Container fluid>
-        <h4 className="mt-2 sidebar-title">Upcoming Events</h4>
-      </Container>
-      <Container>
-        <Row>
-          <Accordion>
-            {events?.map((event) => (
-              <Accordion.Item
-                eventKey={event?._id}
-                key={event?._id}
-                style={{ backgroundColor: event?.assignedColor }}
-              >
-                <Accordion.Header>
-                  <span className="ecplise-text">{event?.title}</span>
-                </Accordion.Header>
-                <Accordion.Body>{event?.description}</Accordion.Body>
-              </Accordion.Item>
-            ))}
-          </Accordion>
-        </Row>
-        <br />
-        <Row>
-          <h4 className="mt-2 sidebar-title">Past Events</h4>
-          <Accordion>
-            {events?.map((event) => (
-              <Accordion.Item
-                eventKey={event?._id}
-                key={event?._id}
-                style={{ backgroundColor: event?.assignedColor }}
-              >
-                <Accordion.Header>
-                  <span className="ecplise-text">{event?.title}</span>
-                </Accordion.Header>
-                <Accordion.Body>{event?.description}</Accordion.Body>
-              </Accordion.Item>
-            ))}
-          </Accordion>
-        </Row>
-      </Container>
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <Container>
+          <Row className="sidebar-section">
+            <h4 className=" sidebar-title">Upcoming Events</h4>
+            {upcomingEvents(events)}
+          </Row>
+          <Row className="sidebar-section">
+            <h4 className=" sidebar-title">Past Events</h4>
+            {pastEvents(events)}
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
